a9a1412fd1b81f099e12482f4dd40af050b1c239
beksultanmusaev <musaeviosdev@gmail.com>
2024-09-04 13:54:12 +0600

    Initial Commit

---
+++ GitHubAccountApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubAccountApp
+//
+//  Created by Бексултан Мусаев on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
